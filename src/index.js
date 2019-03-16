5c8f948591f9a467331e8df311696e8f668b0683
Juan Pablo Peveri Rodriguez <juampi.peveri@gmail.com>
2019-03-16 14:58:14 -0300

    removed config from the repo

--- src/index.js
+++ src/index.js
@@ -3,16 +3,9 @@ import ReactDOM from 'react-dom';
 import firebase from 'firebase';
 import './index.css';
 import App from './App';
+import config from './config/firebaseConfig';
 
-// Initialize Firebase
-const config = {
-  apiKey: 'AIzaSyBMqWqXNAyRiYlaWXj_5irUXs8GTSfj11s',
-  authDomain: 'my-expenses-deeae.firebaseapp.com',
-  databaseURL: 'https://my-expenses-deeae.firebaseio.com',
-  projectId: 'my-expenses-deeae',
-  storageBucket: 'my-expenses-deeae.appspot.com',
-  messagingSenderId: '953941569853'
-};
+// import the config from your firebase app
 firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
